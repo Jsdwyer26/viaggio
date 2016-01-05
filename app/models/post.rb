8886ea6fbd038af7f316841269c292fd1a8590a5
stanleyyork <stanleyyork@gmail.com>
2016-01-05 15:28:34 -0800

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :city
+	validates_length_of :title, :within => 1..200
+	validates_length_of :title, :minimum => 1
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>"}, default_url: "http://www.riverstagetheatre.org/wp-content/uploads/2012/06/missing.png" 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	validates_presence_of :email, :notice => "Need an email"
+	validates_uniqueness_of :email, :notice => "Email already taken"
+	validates_presence_of :name, :notice => "Need a name"
+	validates_uniqueness_of :name, :notice => "Name already taken"
 end
