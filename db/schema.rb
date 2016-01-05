91a1d3e0569324f561a7797c58f15b570e53c5a5
DreTGiant <amassey30@yahoo.com>
2016-01-04 18:36:33 -0800

    current city function added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104222745) do
+ActiveRecord::Schema.define(version: 20160105013243) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160104222745) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "current_city"
   end
 
 end
