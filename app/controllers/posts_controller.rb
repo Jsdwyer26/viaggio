6e2baa189a725034bcf902c537a4c415c7c836ae
Jsdwyer26 <Jsdwyer26@gmail.com>
2016-01-05 09:14:22 -0800

    added date and some styling to post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
 
   before_filter :authorize, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :post_params
+  #temperary function to set city
+  before_filter :set_city
 
 
   def index
@@ -9,7 +12,8 @@ class PostsController < ApplicationController
 
 
   def show
-
+    #For demo purposes
+    @user = current_user
   end
 
 
@@ -19,9 +23,9 @@ class PostsController < ApplicationController
 
 
   def edit
-    #unless current_user == @post.user 
-      #redirect_to "/users/#{current_user.id}"
-    #end
+    unless current_user == @post.user 
+      redirect_to user_path(@user)
+    end
   end
 
   def create
@@ -68,6 +72,10 @@ end
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params[:post]
+      post_id = params[:id]
+      @post = Post.find_by_id(post_id)
+    end
+    def set_city
+      @city = "Miami"
     end
 end
