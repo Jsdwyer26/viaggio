6e1641359fee49c0f91263d45c07fc1441803d08
stanleyyork <stanleyyork@gmail.com>
2016-01-06 11:39:32 -0800

    added admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105185738) do
+ActiveRecord::Schema.define(version: 20160106193111) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160105185738) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.boolean  "admin"
   end
 
 end
